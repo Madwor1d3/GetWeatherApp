a8f4aeb1b5d12a26cc91ed30872617d7fd8d879d
Dmitry <kashlevdmitry@gmail.com>
2019-02-12 20:16:34 +0300

    Added ChangeCityVC and GetWeatherVC

---
+++ GetWeather/Controller/ChangeCityVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeCityVC.swift
+//  GetWeather
+//
+//  Created by Madwor1d3 on 12/02/2019.
+//  Copyright © 2019 Madwor1d3. All rights reserved.
+//
+
+import UIKit
+
+class ChangeCityVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ GetWeather/Controller/WeatherVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetWeather
+//
+//  Created by Madwor1d3 on 12/02/2019.
+//  Copyright © 2019 Madwor1d3. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
