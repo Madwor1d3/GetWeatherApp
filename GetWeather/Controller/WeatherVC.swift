3a8654400bf4df18c0fbafc835ee45012db4e9cd
Dmitry <kashlevdmitry@gmail.com>
2019-02-12 22:54:02 +0300

    Methods updated

--- GetWeather/Controller/WeatherVC.swift
+++ GetWeather/Controller/WeatherVC.swift
@@ -7,14 +7,91 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, CLLocationManagerDelegate {
+    
+    
+    let WEATHER_URL = "https://api.darksky.net/forecast/[key]/[latitude],[longitude]"
+    let APP_ID = "0003c8b808e3ed25e649612abf659184"
+    let locationManager = CLLocationManager()
+    
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self //Принимает делигирование на себя
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
 
+    @IBAction func changeViewsButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    //Mark: - Networking
+    /***************************************************************/
+    
+    func getWeatherData(url: String, parameters: [String: String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                
+                print("Success! Got the weather data")
+                let weatherJSON : JSON = JSON(response.result.value!)
+                
+                
+                print(weatherJSON)
+                
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+        
+    }
+    
+    
+    //Mark: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[locations.count - 1] //Когда CoreLocation ищет информацию, то помещает данные в массив [CLLocation], последний индекс - самая точная инфа
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location   Unavailable"
+    }
 }
 
